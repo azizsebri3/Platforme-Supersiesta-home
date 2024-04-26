a0a2bce8b41bc0da866890691282e212e1dfe08e
Aziz Sebri <sebriaziz2016@gmail.com>
2024-04-26 11:41:41 +0000

    Update loading spinner component

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -4,7 +4,7 @@ import "../output.css";
 const Loading = ({active}) => {
   return (
     <div className={`loading-spinner-overlay ${active ? 'active' : ''}`}>
-      <div class="rounded-full h-10 w-10 bg-[#A2BA02] animate-ping"></div>
+      <div class="spinner"></div>
     </div>
   );
 };
